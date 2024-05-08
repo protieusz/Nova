f9df61d852bccbaa5b35c8f17a382ad0a61427e8
ProtieusKeebs <118025702+protieusz@users.noreply.github.com>
2024-05-08 08:40:00 -0700

    Update nova.c

--- nova/nova.c
+++ nova/nova.c
@@ -12,8 +12,9 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
+ *
+ * Core code borrowed from aki27 from bally, no code change has been made/
+    
 #include "quantum.h"
 #include <math.h>
 #include "nova.h"
